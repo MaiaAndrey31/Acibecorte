7240d1e94b6ae83e78e89261c5a81e1403de6ad5
MaiaAndrey31 <maiaandrey31@outlook.com>
2024-03-15 10:32:24 -0300

    FeatStyleAdjust

--- src/components/ButtonWhats/style.js
+++ src/components/ButtonWhats/style.js
@@ -1,43 +1,39 @@
- import {styled} from 'styled-components'
-import { theme } from '../../styles/theme';
+import { styled } from 'styled-components'
+import { theme } from '../../styles/theme'
 
- export const Container = styled.div`
- padding-top: 2rem;
- a {
+export const Container = styled.div`
+  padding-top: 2rem;
+  a {
     text-decoration: none;
-    
   }
- 
- `
- export const ButtonIcon = styled.div`
- width: auto;
- height: auto;
- display: flex;
- gap: .5rem;
- flex-direction: row-reverse;
- align-items: center;
- justify-content: center;
- background-color: ${theme.colors.success};
- border: .125rem solid ${theme.colors.white};
- border-radius: 1.5rem;
- padding: .9rem .5rem;
- transition: all ease-in-out 200ms;
+`
+export const ButtonIcon = styled.div`
+  width: auto;
+  height: auto;
+  display: flex;
+  gap: 0.5rem;
+  flex-direction: row-reverse;
+  align-items: center;
+  justify-content: center;
+  background-color: ${theme.colors.success};
+  border: 0.125rem solid ${theme.colors.white};
+  border-radius: 1.5rem;
+  padding: 0.9rem 0.5rem;
+  transition: all ease 500ms;
 
- .icon {
-   color: ${theme.colors.white};
-   font-size: 2.4rem;
-   
-   
- }
+  .icon {
+    color: ${theme.colors.white};
+    font-size: 2.4rem;
+  }
 
- h5 {
-   font-size: 1.5rem;
-   color: ${theme.colors.white};
-   font-weight: 600;
- }
- &:hover {
-   cursor: pointer;
-   opacity: 0.8;
-   transform: scale(.98);
- }
+  h5 {
+    font-size: 1.5rem;
+    color: ${theme.colors.white};
+    font-weight: 600;
+  }
+  &:hover {
+    cursor: pointer;
+    opacity: 0.9;
+    transform: scale(0.9);
+  }
 `

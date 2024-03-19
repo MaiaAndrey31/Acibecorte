4ba4e0bea23c5847b27635053d43813943d88ace
MaiaAndrey31 <maiaandrey31@outlook.com>
2024-03-19 20:59:36 -0300

    FeatWhatsPulse

--- src/components/ButtonWhatsPulse/index.jsx
+++ src/components/ButtonWhatsPulse/index.jsx
@@ -1,13 +1,14 @@
-import * as C from './style'
+import './style.css'
 import ImgWhats from '../../assets/icon-withe-whatsapp.svg'
 
 function BtnWhatsPulse() {
     return (
-    <C.LinkWhats>
-        <C.BtnWhats className="pulse">
-            <img src={ImgWhats} />
-        </C.BtnWhats>
-    </C.LinkWhats>
+    
+      <a href="https://wa.me/551145441075" target="_blank">
+        <div class="pulse">
+          <img  src={ImgWhats} alt="" />
+        </div>
+      </a>
     )
 }
 export default BtnWhatsPulse
